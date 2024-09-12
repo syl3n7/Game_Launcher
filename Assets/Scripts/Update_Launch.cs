baf4c6dae8871dbca0deb38edfe81c76ffe4f9b8
Syl3nt ~ Cláudio <claudio.silva365@gmail.com>
2024-09-12 13:49:58 +0100

    should be able to download something now

--- Assets/Scripts/Update_Launch.cs
+++ Assets/Scripts/Update_Launch.cs
@@ -3,33 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEditor.PackageManager;
 using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
+using UnityEngine.Networking;
 
 public class Update_Launch : MonoBehaviour
 {
     [SerializeField] private Button launchBtn;
+    string URL = "https://github.com/syl3n7/MyLittleExploree/releases/download/0.54/MyLittleExploreeV0.54.zip";
+    public Text Error;
     void Awake()
     {
         //Makes the button to change its function to be launching the executable of the game.
         Button btn = launchBtn.GetComponent<Button>();
         btn.onClick.AddListener(Launch);
+        Debug.developerConsoleEnabled = true;
     }
 
     void Launch()
     {
+        Debug.Log("Checking for update!");
+        string launchPath = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf('/'));
+        StartCoroutine(Exec_Updater(URL, launchPath));
         Debug.Log("Launch Success!");
-        Process.Start("cmd.exe", "/c start launch");
-        //https://github.com/syl3n7/MyLittleExploree/releases/download/0.54/MyLittleExploreeV0.54.zip
-    }
-    
-    void Start()
-    {
-        
+        //Process.Start("la", "");
     }
 
-    void Update()
+    private IEnumerator Exec_Updater(string URL, string launchPath)
     {
+        //Download and extract a zip file from somewhere and then call the extract funcion.
+
+        UnityWebRequest last_version = UnityWebRequest.Get(URL);
+        yield return last_version.SendWebRequest();
+        if (last_version.result != UnityWebRequest.Result.Success) Error.text = "Communication Error: "+ last_version.error;
+        else
+        {
+            Debug.Log(last_version.downloadHandler.text);
+        }
         
     }
+    
 }
