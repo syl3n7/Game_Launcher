ee2037bb940bf6d6a44a338fbc201cccf0c64972
Syl3nt ~ Cláudio <claudio.silva365@gmail.com>
2024-09-15 13:10:14 +0100

    self update process almost complete

--- Assets/Scripts/Update_Launch.cs
+++ Assets/Scripts/Update_Launch.cs
@@ -18,51 +18,63 @@ public class Update_Launch : MonoBehaviour
     [SerializeField] private Text error_text;
     [SerializeField] private Text progress_text;
 
-    string URL = "https://github.com/syl3n7/MyLittleExploree/releases/download/0.54/MyLittleExploreeV0.54.zip";
-
+    private string app_URL = "https://steelchunk.eu/releases/latest.zip";
+    private string app_zip_path = Application.streamingAssetsPath + "/latest.zip";
+    private string app_folder_path = Application.streamingAssetsPath + "/";
+    private string app_exe_path = Application.streamingAssetsPath + "/Game_Launcher.exe";
+    private string game_URL = "https://github.com/syl3n7/MyLittleExploree/releases/download/0.54/MyLittleExploreeV0.54.zip";
+    private string game_zip_path = Application.streamingAssetsPath + "/MyLittleExploreeV0.54.zip";
+    private string game_folder_path = Application.streamingAssetsPath + "/MyLittleExploree";
+    private string game_exe_path = Application.streamingAssetsPath + "/MyLittleExploree/MyLittleExploreeV0.54.exe";
+        
     void Awake()
     {
         //Makes the button to change its function to be launching the executable of the game.
         Button btn = downloadBtn.GetComponent<Button>();
-        btn.onClick.AddListener(Launch);
+        btn.onClick.AddListener(game_Update);
         Debug.developerConsoleEnabled = true;
         Debug.developerConsoleVisible = true;
         Button btn2 = playBtn.GetComponent<Button>();
-        btn2.onClick.AddListener(ExecuteNow);
+        btn2.onClick.AddListener(executeNow);
     }
 
-    void ExecuteNow()
+    void Start()
     {
-        Process.Start("Resources/MyLittleExploree/MyLittleExploree.exe");
+        self_update();
     }
-
-    void Launch()
+    
+    void self_update()
     {
         Debug.Log("Self-update!");
-        string launchPath = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf('/'));
-        StartCoroutine(Exec_Updater(URL, launchPath));
+        StartCoroutine(exec_Updater(app_URL, app_zip_path, app_folder_path));
+    }
+    
+    void game_Update()
+    {
+        Debug.Log("updating game!");
+        StartCoroutine(exec_Updater(game_URL, game_zip_path, game_folder_path));
+    }
+    
+    void executeNow()
+    {
+        Debug.Log("execute now!");
+        Process.Start(game_exe_path);
     }
 
-    private IEnumerator Exec_Updater(string URL, string launchPath)
+    private IEnumerator exec_Updater(string URL, string zip_path, string folder_path)
     {
         //Download and extract a zip file from somewhere and then call the extract funcion.
 
         UnityWebRequest last_version = UnityWebRequest.Get(URL);
         yield return last_version.SendWebRequest();
-        progress_text.text = last_version.downloadedBytes.ToString();
-        yield return new WaitForSecondsRealtime(1); 
-        progress_text.text = last_version.downloadedBytes.ToString();
-        yield return new WaitForSecondsRealtime(1); 
-        progress_text.text = last_version.downloadedBytes.ToString();
-        yield return new WaitForSecondsRealtime(1); 
 
         if (!last_version.downloadHandler.isDone) error_text.text = "Communication Error: "+ last_version.error;
         else 
         {
 
-            File.WriteAllBytes("Resources/downloaded.zip", last_version.downloadHandler.data);
+            File.WriteAllBytes(zip_path, last_version.downloadHandler.data);
             
-            ZipFile.ExtractToDirectory("Resources/downloaded.zip", "Resources/MyLittleExploree");
+            ZipFile.ExtractToDirectory(zip_path, folder_path);
 
             playBtn.gameObject.GetComponent<Button>().interactable = true;
             downloadBtn.gameObject.GetComponent<Button>().interactable = false;
