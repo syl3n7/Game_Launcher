0ef04b0ab26cde6c2983887c1a63f55cc2030793
claudio <claudiosilva@steelchunk.eu>
2024-09-15 21:07:26 +0100

    most of the functionality is working, need to fix folder already exists

--- Assets/Scripts/Update_Launch.cs
+++ Assets/Scripts/Update_Launch.cs
@@ -18,68 +18,73 @@ public class Update_Launch : MonoBehaviour
     [SerializeField] private Text error_text;
     [SerializeField] private Text progress_text;
 
-    private string app_URL = "https://steelchunk.eu/releases/latest.zip";
-    private string app_zip_path = Application.streamingAssetsPath + "/latest.zip";
-    private string app_folder_path = Application.streamingAssetsPath + "/";
-    private string app_exe_path = Application.streamingAssetsPath + "/Game_Launcher.exe";
-    private string game_URL = "https://github.com/syl3n7/MyLittleExploree/releases/download/0.54/MyLittleExploreeV0.54.zip";
-    private string game_zip_path = Application.streamingAssetsPath + "/MyLittleExploreeV0.54.zip";
-    private string game_folder_path = Application.streamingAssetsPath + "/MyLittleExploree";
-    private string game_exe_path = Application.streamingAssetsPath + "/MyLittleExploree/MyLittleExploreeV0.54.exe";
-        
+    //private string app_URL = "https://steelchunk.eu/launcher/releases/latest.zip";
+    private string game_URL = "https://steelchunk.eu/games/MyLittleExploree/releases/latest.zip";
+    private string game_zip_path = Application.dataPath + "/MyLittleExploreeV0.54.zip";
+    private string game_folder_path = Application.dataPath + "/MyLittleExploree";
+    private string game_exe_path = Application.dataPath + "/MyLittleExploree/MyLittleExploreeV0.54.exe";
+
     void Awake()
     {
-        //Makes the button to change its function to be launching the executable of the game.
-        Button btn = downloadBtn.GetComponent<Button>();
-        btn.onClick.AddListener(game_Update);
         Debug.developerConsoleEnabled = true;
         Debug.developerConsoleVisible = true;
+        Button btn = downloadBtn.GetComponent<Button>();
+        btn.onClick.AddListener(check_for_update);
+        downloadBtn.gameObject.GetComponent<Button>().interactable = false;
         Button btn2 = playBtn.GetComponent<Button>();
         btn2.onClick.AddListener(executeNow);
+        playBtn.gameObject.GetComponent<Button>().interactable = false;
     }
 
     void Start()
     {
-        self_update();
+        check_for_update();
     }
-    
-    void self_update()
+
+    //instead of trying to update myself, i just tell the user if theres a new version to be downloaded
+    void check_for_update()
     {
-        Debug.Log("Self-update!");
-        StartCoroutine(exec_Updater(app_URL, app_zip_path, app_folder_path));
-    }
-    
-    void game_Update()
-    {
-        Debug.Log("updating game!");
         StartCoroutine(exec_Updater(game_URL, game_zip_path, game_folder_path));
     }
-    
+
     void executeNow()
     {
-        Debug.Log("execute now!");
         Process.Start(game_exe_path);
     }
 
     private IEnumerator exec_Updater(string URL, string zip_path, string folder_path)
     {
         //Download and extract a zip file from somewhere and then call the extract funcion.
-
-        UnityWebRequest last_version = UnityWebRequest.Get(URL);
-        yield return last_version.SendWebRequest();
-
-        if (!last_version.downloadHandler.isDone) error_text.text = "Communication Error: "+ last_version.error;
-        else 
+        if (URL == game_URL)
         {
+            UnityWebRequest last_version = UnityWebRequest.Get(URL);
+            yield return last_version.SendWebRequest();
+            progress_text.text = last_version.downloadProgress.ToString();
 
-            File.WriteAllBytes(zip_path, last_version.downloadHandler.data);
-            
-            ZipFile.ExtractToDirectory(zip_path, folder_path);
+            if (!last_version.downloadHandler.isDone)
+            {
+                error_text.text = "Communication Error: " + last_version.error;
+                downloadBtn.gameObject.GetComponent<Button>().interactable = true;
+            }
+            else
+            {
+                if (last_version.downloadHandler.isDone && last_version.downloadHandler.nativeData.Length > 0)
+                {
+                    if (File.Exists(folder_path)) File.Delete(folder_path);
+                    else File.Create(folder_path);
 
-            playBtn.gameObject.GetComponent<Button>().interactable = true;
-            downloadBtn.gameObject.GetComponent<Button>().interactable = false;
+                    File.WriteAllBytes(zip_path, last_version.downloadHandler.data);
 
+                    ZipFile.ExtractToDirectory(zip_path, folder_path);
+
+                    playBtn.gameObject.GetComponent<Button>().interactable = true;
+                    downloadBtn.gameObject.GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+        else
+        {
+            Application.Quit();
         }
     }
-    
 }
